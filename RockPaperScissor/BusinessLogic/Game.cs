c7f2768d6a00491c7a794968eec6db624a8bc2e6
Bogdan-Strat <bodo.pa2001@yahoo.com>
2023-09-17 21:40:10 +0300

    Select the weapon from the user

--- RockPaperScissor/BusinessLogic/Game.cs
+++ RockPaperScissor/BusinessLogic/Game.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RockPaperScissor.ExtensionMethods;
+using RockPaperScissor.Resources;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,11 +12,46 @@ namespace RockPaperScissor.BusinessLogic
     {
         public int UserWeapon { get; set; }
         public int ComputerWeapon { get; set; }
+        public static int GameRound { get; set; } = 1;
 
         public void GenerateComputerWeapon()
         {
             var random = new Random();
             ComputerWeapon = random.Next(1, 4);
+        }
+
+        public bool IsWeaponValid(string weapon)
+        {
+            foreach(var enumWeapon in Enum.GetNames(typeof(Weapon)))
+            {
+                if(weapon.ToLower() == enumWeapon.ToLower())
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public void SelectTheWeaponFromTheUser()
+        {
+            while (true)
+            {
+                Console.WriteLine($"Round {GameRound} - choose your weapon: ");
+                var weapon = Console.ReadLine();
+
+                if (weapon != null && IsWeaponValid(weapon))
+                {
+                    UserWeapon = (int)(Weapon)Enum.Parse(typeof(Weapon), weapon.ToLower().CapitalizeFirstLetter());
+                    Console.WriteLine(UserWeapon);
+                    GameRound++;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Weapon is invalid. Please select again.\nChoose your weapon");
+                }
+            }
         }
     }
 }
---
+++ RockPaperScissor/ExtensionMethods/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissor.ExtensionMethods
+{
+    public static class StringExtensions
+    {
+        public static string CapitalizeFirstLetter(this string str)
+        {
+            return $"{char.ToUpper(str[0])}{str.Substring(1)}";
+        }
+    }
+}
