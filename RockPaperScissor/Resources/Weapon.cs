b830d2717a8bfd5470f88b1cdc4c9e7602f0deb7
Bogdan-Strat <bodo.pa2001@yahoo.com>
2023-09-17 20:59:33 +0300

    Add weapon enum

---
+++ RockPaperScissor/Resources/Weapon.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissor.Resources
+{
+    public enum Weapon : int
+    {
+        Rock = 1,
+        Paper = 2,
+        Scissor = 3
+    }
+}
