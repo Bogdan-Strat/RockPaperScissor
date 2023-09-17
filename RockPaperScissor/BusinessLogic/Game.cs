84f397a5302dbd1b28e824a6667a4077f9cd0358
Bogdan-Strat <bodo.pa2001@yahoo.com>
2023-09-17 21:09:42 +0300

    Create generate weapon for computer method

---
+++ RockPaperScissor/BusinessLogic/Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissor.BusinessLogic
+{
+    public class Game
+    {
+        public int UserWeapon { get; set; }
+        public int ComputerWeapon { get; set; }
+
+        public void GenerateComputerWeapon()
+        {
+            var random = new Random();
+            ComputerWeapon = random.Next(1, 4);
+        }
+    }
+}
